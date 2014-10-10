9368e20ae8d1c4d9782e526afbc16459ca8d82b1
mahbub-shohag <shohag.cse3@gmail.com>
2014-10-10 10:43:49 -0700

    My first file added to github.

---
+++ c# programs/formFillUp/formFillUp/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace formFillUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxFathersName = new System.Windows.Forms.TextBox();
+            this.textBoxMotersName = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textinfroll = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(119, 31);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(202, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxFathersName
+            // 
+            this.textBoxFathersName.Location = new System.Drawing.Point(119, 81);
+            this.textBoxFathersName.Name = "textBoxFathersName";
+            this.textBoxFathersName.Size = new System.Drawing.Size(202, 20);
+            this.textBoxFathersName.TabIndex = 2;
+            // 
+            // textBoxMotersName
+            // 
+            this.textBoxMotersName.Location = new System.Drawing.Point(119, 130);
+            this.textBoxMotersName.Name = "textBoxMotersName";
+            this.textBoxMotersName.Size = new System.Drawing.Size(202, 20);
+            this.textBoxMotersName.TabIndex = 4;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(39, 292);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(98, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(164, 186);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(14, 13);
+            this.radioMale.TabIndex = 6;
+            this.radioMale.TabStop = true;
+            this.radioMale.UseVisualStyleBackColor = true;
+          
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(274, 186);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(14, 13);
+            this.radioFemale.TabIndex = 7;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.UseVisualStyleBackColor = true;
+    
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "sex :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(440, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(34, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ID NO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Male";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Female";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Name :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "FathersName :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "MothersName: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textinfroll
+            // 
+            this.textinfroll.Location = new System.Drawing.Point(334, 301);
+            this.textinfroll.Name = "textinfroll";
+            this.textinfroll.Size = new System.Drawing.Size(100, 20);
+            this.textinfroll.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(215, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Type Your Id. Please";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 457);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textinfroll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxMotersName);
+            this.Controls.Add(this.textBoxFathersName);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxFathersName;
+        private System.Windows.Forms.TextBox textBoxMotersName;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textinfroll;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ c# programs/formFillUp/formFillUp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formFillUp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] name = new string[30];
+        string[] fathersName = new string[30];
+        string[] mothersName = new string[30];
+        string[] sex = new string[30];
+        int[] idNumber = new int[30];
+        int i = 0;
+
+       
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+           
+            i = Convert.ToInt32(comboBox1.Text);
+            idNumber[i] = Convert.ToInt32(comboBox1.Text);
+            name[i]=textBoxName.Text;
+            fathersName[i]=textBoxFathersName.Text;
+            mothersName[i] = textBoxMotersName.Text;
+            if(radioMale.Checked==true)
+            {
+                sex[i] = "Male";
+            }
+            else
+            {
+                sex[i] = "Female";
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int infid=0;
+            infid = Convert.ToInt16(textinfroll.Text);
+            MessageBox.Show("Name=" + name[infid] + "\nId No.=" + idNumber[infid] + "\nFather's Name=" + fathersName[infid] + "\nmothers Name=" + mothersName[infid] + "\nSex=" + sex[infid]);
+        }
+       
+    }
+}

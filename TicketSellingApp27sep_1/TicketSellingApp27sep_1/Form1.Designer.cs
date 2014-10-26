4bd96e8891894ac373d28513a154e88b180b992a
Mahbub Alam Shohag <shohag.cse3@gmail.com>
2014-10-26 12:35:48 +0600

    TicketSellingApp 27 sep 1st practice.

---
+++ TicketSellingApp27sep_1/TicketSellingApp27sep_1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TicketSellingApp27sep_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.customrNameTextBox = new System.Windows.Forms.TextBox();
+            this.TicketQuantityTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.showDetailButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of tickets";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Per ticket 10 taka";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showDetailButton);
+            this.groupBox1.Controls.Add(this.TicketQuantityTextBox);
+            this.groupBox1.Controls.Add(this.okButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.customrNameTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 165);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ticcket purchase Info";
+            // 
+            // customrNameTextBox
+            // 
+            this.customrNameTextBox.Location = new System.Drawing.Point(103, 39);
+            this.customrNameTextBox.Name = "customrNameTextBox";
+            this.customrNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.customrNameTextBox.TabIndex = 0;
+            // 
+            // TicketQuantityTextBox
+            // 
+            this.TicketQuantityTextBox.Location = new System.Drawing.Point(103, 76);
+            this.TicketQuantityTextBox.Name = "TicketQuantityTextBox";
+            this.TicketQuantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TicketQuantityTextBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(13, 132);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "Save";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // showDetailButton
+            // 
+            this.showDetailButton.Location = new System.Drawing.Point(128, 132);
+            this.showDetailButton.Name = "showDetailButton";
+            this.showDetailButton.Size = new System.Drawing.Size(75, 23);
+            this.showDetailButton.TabIndex = 3;
+            this.showDetailButton.Text = "Details";
+            this.showDetailButton.UseVisualStyleBackColor = true;
+            this.showDetailButton.Click += new System.EventHandler(this.showDetailButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 180);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showDetailButton;
+        private System.Windows.Forms.TextBox TicketQuantityTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox customrNameTextBox;
+    }
+}
+
---
+++ TicketSellingApp27sep_1/TicketSellingApp27sep_1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketSellingApp27sep_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string custumerName;
+        private int ticketQuantity;
+        private double totalAmount;
+        private const int perTiccketPrice = 10;
+        private void okButton_Click(object sender, EventArgs e)
+        {
+           custumerName = customrNameTextBox.Text;
+           ticketQuantity = int.Parse(TicketQuantityTextBox.Text);
+            totalAmount = perTiccketPrice*ticketQuantity;
+        }
+
+        private void showDetailButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mr."+custumerName +" please pay :"+totalAmount+"tk for "+ticketQuantity +" tickets") ;
+          clear();
+
+        }
+
+        void clear()
+        {
+            customrNameTextBox.Text = "";
+            TicketQuantityTextBox.Text = "";
+
+        }
+    }
+}
